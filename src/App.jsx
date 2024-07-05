abbe20384fb3cbc9f049124f941326d722831019
sonja <sonja-dahlke@web.de>
2024-07-05 15:34:25 +0200

    add logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,45 @@ import { Verbs } from "./assets/Verbs";
 import { Navbar } from "../src/Components/Navbar";
 
 function App() {
-  const [word, setWord] = useState("");
+  const [words, setWords] = useState(generateAllWords);
 
-  let verb = Math.floor(Math.random() * Verbs.length + 1);
-  let noun = Math.floor(Math.random() * Nouns.length + 1);
-  let adjective = Math.floor(Math.random() * Adjectives.length + 1);
+  function generateWord(index) {
+    if (index === 0 || index === 3) {
+      return Adjectives[Math.floor(Math.random() * Adjectives.length + 1)];
+    }
+    if (index === 1 || index === 4) {
+      return Nouns[Math.floor(Math.random() * Nouns.length - 1)];
+    }
+    return Verbs[Math.floor(Math.random() * Verbs.length - 1)];
+  }
+
+  function generateAllWords() {
+    let word1 = Adjectives[Math.floor(Math.random() * Adjectives.length + 1)];
+    let word2 = Nouns[Math.floor(Math.random() * Nouns.length - 1)];
+    let word3 = Verbs[Math.floor(Math.random() * Verbs.length - 1)];
+    let word4 = Adjectives[Math.floor(Math.random() * Adjectives.length + 1)];
+    let word5 = Nouns[Math.floor(Math.random() * Nouns.length - 1)];
+    return [word1, word2, word3, word4, word5];
+  }
+
+  function handleGenerator() {
+    setWords(generateAllWords());
+  }
 
   return (
     <>
       <Navbar />
       <div className="container">
         <Sentence
-          adjectiveOne="Bli"
-          nounOne="Bla"
-          verb="Blub"
-          adjectiveTwo="Möp"
-          nounTwo="Muh"
+          words={words}
+          onSelectWord={(index) => {
+            setWords(
+              words.map((word, i) => (i === index ? generateWord(index) : word))
+            );
+            console.log(index);
+          }}
         />
-
-        <button>Generiere!</button>
+        <button onClick={handleGenerator}>Generiere!</button>
 
         <div>?</div>
       </div>
--- src/Components/Sentence.jsx
+++ src/Components/Sentence.jsx
@@ -2,15 +2,26 @@ import { Adjectives } from "../assets/Adjectives";
 import { Nouns } from "../assets/Nouns";
 import { Verbs } from "../assets/Verbs";
 import "../App.css";
+import "./Sentence.css";
 
-export function Sentence(props) {
+export function Sentence({ words, onSelectWord }) {
   return (
     <div className="generator-container">
-      <span className="word adjectiveOne">{props.adjectiveOne}</span>
-      <span className="word nounOne">{props.nounOne}</span>
-      <span className="word verb">{props.verb}</span>
-      <span className="word adjectiveTwo">{props.adjectiveTwo}</span>
-      <span className="word nounTwo">{props.nounTwo}</span>
+      <span onClick={() => onSelectWord(0)} className="word adjectiveOne">
+        {words[0]}
+      </span>
+      <span onClick={() => onSelectWord(1)} className="word nounOne">
+        {words[1]}
+      </span>
+      <span onClick={() => onSelectWord(2)} className="word adjectiveTwo">
+        {words[2]}
+      </span>
+      <span onClick={() => onSelectWord(3)} className="word verb">
+        {words[3]}
+      </span>
+      <span onClick={() => onSelectWord(4)} className="word nounTwo">
+        {words[4]}
+      </span>
     </div>
   );
 }
